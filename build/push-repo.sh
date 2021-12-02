3058ceb75b8bc629e36afc099f9063116fbcc40e
Morten Bork <mmb@kvalitetsit.dk>
2021-12-02 12:32:52 +0100

    Update helm push

--- build/push-repo.sh
+++ build/push-repo.sh
@@ -8,6 +8,7 @@ git config --global user.name "Github Actions"
 git config --global user.email "development@kvalitetsit.dk"
 git clone https://github.com/KvalitetsIT/helm-repo
 cd helm-repo
+mkdir -p openid
 cp ../../openid-*.tgz openid
 helm repo index . --url https://raw.githubusercontent.com/KvalitetsIT/helm-repo/master/
 git add -A
